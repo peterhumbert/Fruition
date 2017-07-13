d62886f7a054f2cc201d94e9ad2517cab9265dc1
Peter Humbert <humbe037@umn.edu>
2017-07-12 21:33:14 -0500

    add preliminary Project class

---
+++ Fruition/Project.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fruition
+{
+    class Project
+    {
+        private DateTime startDate { get; set; }
+        private string name { get; set; }
+        private string status { get; set; }
+        private DateTime lastActive { get; set; }
+        private string lastActiveActivity { get; set; }
+        // record of activities
+        private string category { get; set; }
+
+        public int getDaysSinceLastActive()
+        {
+            return lastActive.Day - startDate.Day;
+        }
+
+        public int getDaysElapsed()
+        {
+            return DateTime.Now.Day - startDate.Day;
+        }
+    }
+}
